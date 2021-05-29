f04abb5a85284c7769e20fb2029dc6f5c36d454a
adamferreira <adam.ferreira.dc@gmail.com>
2021-05-29 22:40:58 +0200

    Add RemoteConnectorClass

---
+++ pyrc/remote/__init__.py
@@ -0,0 +1,1 @@
+from .remotecon import RemoteConnector
---
+++ pyrc/remote/remotecon.py
@@ -0,0 +1,200 @@
+import paramiko
+from scp import SCPClient, SCPException
+import logging
+import os
+
+# ------------------ RemoteConnector
+class RemoteConnector:
+	def __init__(self, hostname, user, user_config_file, sshkey):
+		# Class members
+		self.hostname = hostname
+		self.user = user
+		self.user_config_file = user_config_file
+		self.sshkey = sshkey
+		self.sshcon = None
+		self.scp = None
+		self.user_config = None
+		self.use_proxy = False
+
+		# Currend directory
+		self.cwd = ""
+
+		# Creating remote connection
+		self.sshcon = paramiko.SSHClient()  # will create the object
+		ssh_config = paramiko.SSHConfig.from_path(self.user_config_file)
+
+		# Creating user config dictionnary
+		self.user_config = ssh_config.lookup(self.hostname)
+
+		# Setting up proxy
+		self.sshcon.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # no known_hosts error
+
+
+	def exec_command(self, cmd, print_output = False, print_input = True):
+		"""[summary]
+			Execute the given command line on the remote machine.
+		Args:
+			cmd (str): Command line to execute.
+			print_output (bool, optional): Boolean to print or not remote stdout to local stdout]. Defaults to False.
+
+		Returns:
+			stdin, stdout, stderr: stdin, stdout, stderr
+		"""
+		if print_input:
+			print("[" + self.user + "@" + self.hostname + "]", cmd)
+		stdin, stdout, stderr = self.sshcon.exec_command("cd " + self.cwd + ";" + cmd)
+		#stdout.channel.recv_exit_status()
+		if print_output:
+			for line in stdout.readlines():
+				print("[" + self.user + "@" + self.hostname + "] ->", line)
+		
+		return stdin, stdout, stderr
+
+	def open(self, use_proxy = False):
+		"""[summary]
+			Opens the remote connection.
+		Args:
+			use_proxy (bool): Specify if the remote connection should go through a proxy.
+		"""
+		self.use_proxy = use_proxy
+		if use_proxy:
+			proxy = paramiko.ProxyCommand(self.user_config["proxycommand"])
+			self.sshcon.connect(self.user_config["hostname"], username=self.user, key_filename=self.sshkey, sock=proxy)
+		else:
+			self.sshcon.connect(self.user_config["hostname"], username=self.user, key_filename=self.sshkey)
+
+		# SCP connection
+		self.scp = SCPClient(self.sshcon.get_transport())
+
+	def is_open(self):
+		try:
+			self.open()
+			return True
+		except :
+			return False
+		
+		return False
+
+	def close(self):
+		"""[summary]
+			Close remote connection.
+		"""
+		if self.sshcon:
+			self.sshcon.close()
+		if self.scp:
+			self.scp.close()
+
+	def ls(self, folderpath):
+		"""[summary]
+			List all remote files and folder found in remote path 'folderpath'.
+		Args:
+			folderpath (str): Remote path to explore.
+
+		Returns:
+			list: List of all the remotes file names.
+		"""
+		outlist = []
+		stdin, stdout, stderr = self.exec_command("ls " + folderpath)
+		for file in stdout.readlines():
+			outlist.append(file.replace('\n', ''))
+		return outlist
+
+	def cd(self, directory):
+		self.cwd = directory
+
+	def pwd(self):
+		return self.cwd
+
+	def remote_file_exists_in_folder(self, folderpath, filename):
+		return filename in self.ls(folderpath)
+
+	def remote_file_exists(self, filepath):
+		splitt = filepath.split("/")
+		filename = splitt[len(splitt)-1]
+		folderpath = ""
+		for p in splitt[:len(splitt)-1]:
+			folderpath += str(p)+'/'
+
+		return self.remote_file_exists_in_folder(folderpath, filename)
+
+	def upload(self, local_file, remote_path):
+		print("Uploading", local_file, "...")
+		upload = None
+		try:
+			self.scp.put(
+				local_file,
+				recursive=True,
+				remote_path = remote_path
+			)
+			upload = local_file
+		except SCPException as error:
+			#logger.error(error)
+			print(error)
+			raise error
+		finally:
+			#logger.info(f'Uploaded {file} to {self.remote_path}')
+			print("Uploaded", local_file, "to", remote_path)
+			return upload		
+
+	def upload_file(self, local_file):
+		return self.upload(local_file, self.cwd)
+
+	def download(self, remote_file_path, local_file_path = "."):
+		"""Recursivly download files/folder from remote host.
+
+		Args:
+			file ([remote_file_path]): Remote file path.
+		"""
+		print("Downloading", remote_file_path, "...")
+		try:
+			self.scp.get(remote_path = remote_file_path, local_path = local_file_path, recursive=True)
+		except SCPException as error:
+			print(error)
+			raise error
+		finally:
+			print("Downloaded", remote_file_path, "to", local_file_path)
+
+	def rm(self, remote_path, flag = ""):
+		stdin, stdout, stderr = self.exec_command("rm " + flag + " " + remote_path)
+
+	def mkdir(self, remote_path):
+		stdin, stdout, stderr = self.exec_command("mkdir " + remote_path)
+
+	def zip(self, remote_path, remote_archive, flag = ""):
+		stdin, stdout, stderr = self.exec_command("zip " + flag + " \"" + remote_archive + "\" \"" + remote_path + "\"", print_output = True)
+		#stdin, stdout, stderr = self.exec_command("zip " + flag + " " + remote_archive + " " + remote_path, print_output = True)
+
+	def unzip(self, remote_archive, remote_path, flag = ""):
+		stdin, stdout, stderr = self.exec_command("unzip " + flag + " \"" + remote_archive + "\" -d \"" + remote_path + "\"", print_output = True)
+
+	def compress_folder(self, remote_folder_path, sep = "/"):
+		if self.remote_file_exists(remote_folder_path):
+			tmpwd = self.cwd
+			self.cd(remote_folder_path)
+			splitt = remote_folder_path.split(sep)
+			remote_archive = splitt[len(splitt)-1] + ".zip"
+			self.zip(".", remote_archive, flag = "-r")
+			self.cd(tmpwd)
+			return remote_folder_path + sep + remote_archive
+		else:
+			raise RuntimeError("Remote folder " + remote_folder_path + " not found.")
+
+	def download_folder(self, remote_folder_path, local_path, compress = False, clean_remote = False):
+
+		if os.path.isdir(local_path):
+			raise RuntimeError("Local path already exists !")
+
+		if self.remote_file_exists(remote_folder_path):
+			if compress:
+				remote_archive_path = self.compress_folder(remote_folder_path)
+				self.download(remote_archive_path, local_path + ".zip")
+				self.rm(remote_archive_path, "-r")
+			else:
+				self.download(remote_folder_path, local_path)
+
+			if clean_remote:
+				self.rm(remote_folder_path, "-r")
+		else:
+			raise RuntimeError("Remote folder " + remote_folder_path + " cannot be found on " + self.hostname + ".")
+
+# ------------------ RemoteConnector
