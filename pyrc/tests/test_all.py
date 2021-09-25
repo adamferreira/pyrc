dccc42f5a4ece8ab6d4b27f51e071f34dceddf15
adamferreira <adam.ferreira.dc@gmail.com>
2021-09-25 17:15:35 +0200

    Basic FileSystem Tests

--- pyrc/tests/test_all.py
+++ pyrc/tests/test_all.py
@@ -1,15 +1,29 @@
-import os
 from .testutils import *
 
-THIS_FILE = os.path.realpath(__file__)
-THIS_DIR = os.path.dirname(THIS_FILE)
 
-def test_filesystem_obj_creation(path):
+def test_filesystem_obj_creation(filesystem):
+    path, is_local = filesystem.path, filesystem.is_local
     assert path is not None
+    
 
 @pytest.mark.depends(on=["test_filesystem_obj_creation"])
-def test_current_location(path):
-    assert path.isfile(THIS_FILE)
-    assert path.isdir(THIS_DIR)
-    assert not path.islink(THIS_FILE)
+def test_filesystem_status(filesystem):
+    path, is_local = filesystem.path, filesystem.is_local
+    assert is_local is not path.is_remote
+    if not is_local:
+        assert path.connector.is_open()
+    else:
+        assert path.connector is None
+
+@pytest.mark.depends(on=["test_filesystem_status"])
+def test_current_location(filesystem):
+    path, workspace = filesystem.path, filesystem.workspace
+    assert path.isdir(workspace)
+    assert not path.islink(workspace)
+
+@pytest.mark.depends(on=["test_current_location"])
+def test_file_creation(filesystem):
+    path, workspace = filesystem.path, filesystem.workspace
+    newfile = path.join(workspace, "newfile.txt")
+    assert not path.isfile(newfile)
     
--- pyrc/tests/testutils.py
+++ pyrc/tests/testutils.py
@@ -1,17 +1,25 @@
 
-import pytest
+import pytest, os
 import pyrc.remote as pyrm
 import pyrc.system as pysys
 import pyrc.event.event as pyevent
 
+THIS_FILE = os.path.realpath(__file__)
+THIS_DIR = os.path.dirname(THIS_FILE)
+
+class FileSystemTest(object):
+    def __init__(self, path:pysys.FileSystem, workspace:str, is_local:bool):
+        self.path = path
+        self.workspace = workspace
+        self.is_local = is_local
 
 FILESYSTEM_OBJECTS = [
-    pysys.FileSystem()
+    FileSystemTest(pysys.FileSystem(), THIS_DIR, True)
 ]
 
 # All test will be called for each connectors !
 @pytest.fixture(params=FILESYSTEM_OBJECTS)
-def path(request):
+def filesystem(request):
     # Current element of FILESYSTEM_OBJECTS array
     # It should be a pyrc FileSystem obejct !
     return request.param 
