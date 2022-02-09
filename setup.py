c5b279716c77d763870dd2496efc57c4e4a74fa7
adamferreira <adam.ferreira.dc@gmail.com>
2022-02-09 22:30:54 +0100

    Change version (test)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyrc",
-    version="0.0.1",
+    version="0.0.2",
     author="Adam Ferreira",
     author_email="adam.ferreira.dc@gmail.com",
     description="Python Remote Computing",
