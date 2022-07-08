5e36f99bd8824608f59869686f466e2ccae4bc4f
adamferreira <adam.ferreira.dc@gmail.com>
2022-07-09 00:04:22 +0200

    Use LocalFileSystem to create connectors instead of os package V2

--- pyrc/remote/remote.py
+++ pyrc/remote/remote.py
@@ -7,12 +7,13 @@ def get_ssh_configurations(user_config_file:str) -> 'dict[str:dict[str:str]]':
     return {hostname : ssh_config.lookup(hostname) for hostname in ssh_config.get_hostnames()}
 
 def create_connectors(user_config_file:str, sshkey:str=None) -> 'dict[str:RemoteSSHFileSystem]':
+    path = LocalFileSystem()
     # Exception if file does not exist
     ssh_configs = get_ssh_configurations(user_config_file)
 
     # If no sshkey specified we add one from the folder where the config file is
     if sshkey is None:
-        sshkey = os.path.join(os.path.dirname(user_config_file), "id_rsa")
+        sshkey = path.join(path.dirname(user_config_file), "id_rsa")
     
     connectors = {}
     for hostname in ssh_configs:
@@ -35,7 +36,7 @@ def create_default_connectors()  -> 'dict[str:RemoteSSHFileSystem]':
     path = LocalFileSystem()
     platform = path.platform()
     if path.is_unix():
-        if "WSL2" in platform.release():
+        if "WSL2" in platform["release"]:
             ssh_config_file = path.join("/mnt" , "c", "Users", path.env("USER"), ".ssh", "config")
         else:
             ssh_config_file = path.join("/home", path.env("USER"), ".ssh", "config")
