7ee9778420a5d34e1f417c75efcbba74fec989e8
Adam Ferreira Da Costa <adam.ferreira.dc@gmail.com>
2022-07-25 13:52:38 +0200

    Update __init__.py

--- pyrc/remote/__init__.py
+++ pyrc/remote/__init__.py
@@ -1,6 +1,6 @@
 try:
     from .sshconnector import RemoteSSHFileSystem
-    from .remote import *
+    from .remote import get_ssh_configurations, create_default_sshconnectors
     _CMDEXEC_REMOTE_ENABLED_ = True
 except BaseException as err:
     _CMDEXEC_REMOTE_ENABLED_ = False
