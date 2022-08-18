278ee2cf0d7462181f7c78e0769596037f57707b
Adam Ferreira Da Costa <adam.ferreira.dc@gmail.com>
2022-08-18 16:38:08 +0200

    Create ssh connector from dict instead of file

--- pyrc/remote/remote.py
+++ pyrc/remote/remote.py
@@ -2,19 +2,43 @@ import paramiko
 from pyrc.system import LocalFileSystem
 from pyrc.remote import RemoteSSHFileSystem
 
+def get_default_keys_paths() -> 'tuple(str,str)':
+	path = LocalFileSystem()
+	platform = path.platform()
+	if path.is_unix():
+		if "WSL2" in platform["release"]:
+			ssh_dir = path.join("/mnt" , "c", "Users", path.env("USER"), ".ssh")
+		else:
+			ssh_dir = path.join("/home", path.env("USER"), ".ssh")
+	else:
+		ssh_dir = path.join("C:\\", "Users", path.env("USER"), ".ssh")
+
+	return path.join(ssh_dir, "id_rsa"), path.join(ssh_dir, "id_rsa.pub")
+
+def get_default_ssh_config() -> 'dict[str:dict[str:str]]':
+	path = LocalFileSystem()
+	platform = path.platform()
+	if path.is_unix():
+		if "WSL2" in platform["release"]:
+			ssh_config_file = path.join("/mnt" , "c", "Users", path.env("USER"), ".ssh", "config")
+		else:
+			ssh_config_file = path.join("/home", path.env("USER"), ".ssh", "config")
+	else:
+		ssh_config_file = path.join("C:\\", "Users", path.env("USER"), ".ssh", "config")
+
+	return get_ssh_configurations(ssh_config_file)
 
 def get_ssh_configurations(user_config_file:str) -> 'dict[str:dict[str:str]]':
 	ssh_config = paramiko.SSHConfig.from_path(user_config_file)
 	return {hostname : ssh_config.lookup(hostname) for hostname in ssh_config.get_hostnames()}
 
-def create_sshconnectors(user_config_file:str, sshkey:str=None) -> 'dict[str:RemoteSSHFileSystem]':
+def create_sshconnectors(ssh_configs:dict, sshkey:str=None) -> 'dict[str:RemoteSSHFileSystem]':
 	path = LocalFileSystem()
-	# Exception if file does not exist
-	ssh_configs = get_ssh_configurations(user_config_file)
 
 	# If no sshkey specified we add one from the folder where the config file is
 	if sshkey is None:
-		sshkey = path.join(path.dirname(user_config_file), "id_rsa")
+		ssh_priv, ssh_pub = get_default_keys_paths()
+		sshkey = ssh_priv
 	
 	connectors = {}
 	for hostname in ssh_configs:
@@ -37,17 +61,7 @@ def create_sshconnectors(user_config_file:str, sshkey:str=None) -> 'dict[str:Rem
 	return connectors
 
 def create_default_sshconnectors()  -> 'dict[str:RemoteSSHFileSystem]':
-	path = LocalFileSystem()
-	platform = path.platform()
-	if path.is_unix():
-		if "WSL2" in platform["release"]:
-			ssh_config_file = path.join("/mnt" , "c", "Users", path.env("USER"), ".ssh", "config")
-		else:
-			ssh_config_file = path.join("/home", path.env("USER"), ".ssh", "config")
-	else:
-		ssh_config_file = path.join("C:\\", "Users", path.env("USER"), ".ssh", "config")
-
-	return create_sshconnectors(ssh_config_file, sshkey=None)
+	return create_sshconnectors(get_default_ssh_config(), sshkey=None)
 
 class RemotePyrc(object):
 	def __init__(self, remote:RemoteSSHFileSystem, python_remote_install:str = None):
