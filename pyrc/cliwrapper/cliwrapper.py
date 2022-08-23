9750f345024388622309facfe7fc678bbfaad8f3
adamferreira <adam.ferreira.dc@gmail.com>
2022-08-23 08:20:46 +0000

    Can now call commands with sourced python venvs

--- pyrc/cliwrapper/cliwrapper.py
+++ pyrc/cliwrapper/cliwrapper.py
@@ -1,5 +1,7 @@
 from pyrc.system import FileSystem, LocalFileSystem
+from pyrc.event import Event, ErrorRaiseEvent, CommandPrettyPrintEvent
 
+# CLI Wrappers are callable objects
 class CLIWrapper(object):
 	connector:FileSystem
 
@@ -9,3 +11,23 @@ class CLIWrapper(object):
 			self.connector = LocalFileSystem()
 		else:
 			self.connector = connector
+
+		# Plug python environ to its connector
+		self.environ = self.connector.environ
+
+	def __call__(self, cmd:str, cwd:str="", event:Event = None):
+		return self.connector.exec_command(
+			cmd = cmd,
+			cwd = cwd,
+			environment = self.environ,
+			event = self.default_event() if event is None else event
+		)
+
+	def default_event(self) -> Event:
+		#return ErrorRaiseEvent(self.connector)
+		return CommandPrettyPrintEvent(
+			self.connector,
+			print_input = True,
+			print_errors = True,
+			use_rich = True
+		)
--- pyrc/cliwrapper/python.py
+++ pyrc/cliwrapper/python.py
@@ -10,6 +10,7 @@ class Python(CLIWrapper):
 			raise RuntimeError(f"Python exe {pyexe} is not a valid path.")
 
 		self.exe = pyexe
+		self.venv = None
 		# Virtual env script path deduction
 		if self.is_venv():
 			venv_prefix = self.prefix()
@@ -17,44 +18,36 @@ class Python(CLIWrapper):
 				self.venv = self.connector.join(venv_prefix, "bin", "activate")
 			else:
 				self.venv = self.connector.join(venv_prefix, "Scripts", "activate")
-		else:
-			self.venv = None
 
-		# Plug python environ to its connector
-		self.environ = self.connector.environ
-
-	def __call__(self, cmd:str, cwd:str=""):
+	def __call__(self, cmd:str, cwd:str="", event:Event = None):
+		"""
+		Calls the command 'cmd' with the python executable
+		"""
 		source_cmd = f"source {self.venv} &&" if self.venv is not None else ""
-		return self.connector.exec_command(
-			cmd = f"{source_cmd} {self.exe} {cmd}",
-			cwd = cwd,
-			environment = self.environ,
-			event = self.default_event()
-		)
+		return CLIWrapper.__call__(self, f"{source_cmd} {self.exe} {cmd}", cwd, event)
 
-	def default_event(self) -> Event:
-		#return ErrorRaiseEvent(self.connector)
-		return CommandPrettyPrintEvent(
-			self.connector,
-			print_input = True,
-			print_errors = True,
-			use_rich = True
-		)
+	def with_venv(self, cmd:str, cwd:str="", event:Event = None):
+		"""
+		Invoque a system command but with the python virtual env sourced first.
+		Note : This do NOT call python.
+		"""
+		source_cmd = f"source {self.venv} &&" if self.venv is not None else ""
+		return CLIWrapper.__call__(self, f"{source_cmd} {cmd}", cwd, event)
 
 	def base_prefix(self) -> str:
 		"""
 		Get the true path of the python exe
 		"""
 		base_prefix_cmd = """getattr(sys, 'base_prefix', None) or getattr(sys, 'real_prefix', None) or sys.prefix"""
-		out, err, status = self.connector.exec_command(
-			cmd = f"{self.exe} -c \"import sys; print({base_prefix_cmd})\"",
-			event = ErrorRaiseEvent(self.connector)
+		out, err, status = self(
+			cmd = f"-c \"import sys; print({base_prefix_cmd})\"",
+			event = ErrorRaiseEvent(self.connector) 
 		)
 		return out[0]
 
 	def prefix(self) -> str:
-		out, err, status = self.connector.exec_command(
-			cmd = f"{self.exe} -c \"import sys; print(sys.prefix)\"",
+		out, err, status = self(
+			cmd = f"-c \"import sys; print(sys.prefix)\"",
 			event = ErrorRaiseEvent(self.connector)
 		)
 		return out[0]
@@ -63,3 +56,5 @@ class Python(CLIWrapper):
 		# https://stackoverflow.com/questions/1871549/determine-if-python-is-running-inside-virtualenv
 		# Check if the python exe belong to a virtual env
 		return self.base_prefix() != self.prefix()
+
+print(Python("/usr/bin/python3").base_prefix())
